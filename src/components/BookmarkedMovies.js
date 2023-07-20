ca5e3a013be3b6f47e8c8ed043ead30078412ad9
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-07-20 18:58:27 +0200

    add: Bookmarked movies CSS

--- src/components/BookmarkedMovies.js
+++ src/components/BookmarkedMovies.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './BookmarkedMovies.css';
 
 function BookmarkedMovies({ movies }) {
     return (
-        <div className='bookmarked-movies'>
-            <h2>Bookmarked Movies</h2>
-            <ul>
+        <div className='bookmarked-movies-div'>
+            <h2 className='bookmarked-movies-title'>Bookmarked Movies</h2>
+            <ul className='bookmarked-movies-list'>
                 {movies.map(movie => (
                     <li key={movie.id}>
-                        <p>{movie.title}</p>
+                        <p className='bookmarked-movie'>{movie.title}</p>
                     </li>
                 ))}
             </ul>
